74c09ad42ab8872970ff0f007d34d357e3b204ba
Harri Suonikko <harri.suonikko@gmail.com>
2019-04-12 11:32:06 +0300

    lisäys

---
+++ osa_2/puhelinluettelo/build/precache-manifest.054c2e8910285aec38aa60402b3ff409.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b9af7412858eb74a2b7",
+    "url": "/static/css/main.d5449800.chunk.css"
+  },
+  {
+    "revision": "8b9af7412858eb74a2b7",
+    "url": "/static/js/main.f9a37d31.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "37cfae212e25180f959b",
+    "url": "/static/js/2.0b74813f.chunk.js"
+  },
+  {
+    "revision": "dd9957d34b6cb274ce701397698f1e24",
+    "url": "/index.html"
+  }
+];
